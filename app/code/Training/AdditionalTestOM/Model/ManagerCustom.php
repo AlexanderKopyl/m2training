d7c82bc7f451a3e1ab06a6109041289482d1ed2e
Kopul@ua.fm <Kopul@ua.fm>
2020-09-13 08:51:54 +0300

    End di.xml part 2

---
+++ app/code/Training/AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Training\AdditionalTestOM\Model;
+
+class ManagerCustom implements \Training\TestOM\Model\ManagerInterface
+{
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function get()
+    {
+        // TODO: Implement create() method.
+    }
+}
--- app/etc/config.php
+++ app/etc/config.php
@@ -296,9 +296,10 @@ return [
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
+        'Training_TestOM' => 1,
         'Training_Dependency2' => 1,
         'Training_Dependency' => 1,
-        'Training_TestOM' => 1,
+        'Training_AdditionalTestOM' => 1,
         'Vertex_Tax' => 1,
         'Vertex_AddressValidation' => 1,
         'Yotpo_Yotpo' => 1
