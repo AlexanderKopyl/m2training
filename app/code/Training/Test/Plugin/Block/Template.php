2396419e25bcf40e9ef2f035a2a90758b437d480
Kopul@ua.fm <Kopul@ua.fm>
2020-09-15 12:12:40 +0300

    End 1.8

---
+++ app/code/Training/Test/Plugin/Block/Template.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Training\Test\Plugin\Block;
+
+class Template
+{
+    public function afterToHtml(
+        \Magento\Framework\View\Element\Template $subject,
+        $result
+    ) {
+
+        if($subject->getTemplate() == 'Magento_Search::form.mini.phtml'){
+            $result = '<div><p>' . $subject->getTemplate() . '</p>' .
+                '<p>' . get_class($subject) . '</p>' . $result . "</div>";
+        }
+
+
+        return $result;
+    }
+}
