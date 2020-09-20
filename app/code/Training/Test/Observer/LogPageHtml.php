4a7b6c1ff0c54d5d2b95af317afcbdd0d3930de0
Kopul@ua.fm <Kopul@ua.fm>
2020-09-20 08:53:48 +0300

    End 2.1 Observr and on controller_front_send_response_before

---
+++ app/code/Training/Test/Observer/LogPageHtml.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\Test\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use \Psr\Log\LoggerInterface;
+
+class LogPageHtml implements ObserverInterface
+{
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * LogPageHtml constructor.
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+
+    }
+
+    public function execute(Observer $observer)
+    {
+        // TODO: Implement execute() method.
+        $response = $observer->getEvent()->getData('response');
+        $this->logger->debug($response->getBody());
+    }
+}
