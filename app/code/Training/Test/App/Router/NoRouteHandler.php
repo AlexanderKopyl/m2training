dd3eeaca96e537e28f038f4c24423e4080fffcc3
Kopul@ua.fm <Kopul@ua.fm>
2020-09-20 12:53:56 +0300

    End 2.3 NoRouteHandlerInterface

---
+++ app/code/Training/Test/App/Router/NoRouteHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Training\Test\App\Router;
+
+
+class NoRouteHandler implements \Magento\Framework\App\Router\NoRouteHandlerInterface
+{
+
+    public function process(\Magento\Framework\App\RequestInterface $request)
+    {
+        // TODO: Implement process() method.
+
+        $moduleName = 'cms';
+        $controllerPath = 'index';
+        $controllerName = 'index';
+
+        $request->setModuleName($moduleName)->setControllerName($controllerPath)->setActionName($controllerName);
+
+        return true;
+    }
+}
