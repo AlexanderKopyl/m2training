81860747f581ca8839142f26ccfd9080a38eeee8
Kopul@ua.fm <Kopul@ua.fm>
2020-09-20 14:30:15 +0300

    End 2.4 RawFactorty 'Simple text'

---
+++ app/code/Training/Test/Controller/Index/Index.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Training\Test\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use \Magento\Framework\Controller\Result\RawFactory;
+
+class Index extends Action implements \Magento\Framework\App\Action\HttpGetActionInterface
+{
+
+    private $resultRawFactory;
+
+
+    public function __construct(
+        Context $context,
+        RawFactory $resultRawFactory
+    )
+    {
+        $this->resultRawFactory = $resultRawFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+//      $this->getResponse()->appendBody('simple Text;');
+
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents('simple Text;');
+
+        return $resultRaw;
+    }
+}
