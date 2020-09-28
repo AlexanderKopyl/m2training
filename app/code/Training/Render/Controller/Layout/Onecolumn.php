903af7887087c3bd9d82df15b48bc06d7aac18b4
Kopul@ua.fm <Kopul@ua.fm>
2020-09-28 19:17:35 +0300

    End 3.11

--- app/code/Training/Render/Controller/Layout/Onecolumn.php
+++ app/code/Training/Render/Controller/Layout/Onecolumn.php
@@ -1,11 +1,8 @@
 <?php
 
-
 namespace Training\Render\Controller\Layout;
 
-
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\View\Result\PageFactory;
 
 class Onecolumn extends \Magento\Framework\App\Action\Action implements \Magento\Framework\App\Action\HttpGetActionInterface
@@ -16,11 +13,15 @@ class Onecolumn extends \Magento\Framework\App\Action\Action implements \Magento
      */
     private $resultPageFactory;
 
+    /**
+     * Onecolumn constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
     public function __construct(
         Context $context,
         PageFactory $resultPageFactory
-    )
-    {
+    ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
     }
