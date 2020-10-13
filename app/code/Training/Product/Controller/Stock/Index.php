0648250fb1255c4a973f469495313b7ddb050250
Kopul@ua.fm <Kopul@ua.fm>
2020-10-13 17:33:44 +0300

    Change for codeshifer

--- app/code/Training/Product/Controller/Stock/Index.php
+++ app/code/Training/Product/Controller/Stock/Index.php
@@ -6,15 +6,27 @@ namespace Training\Product\Controller\Stock;
 
 class Index implements \Magento\Framework\App\Action\HttpPostActionInterface
 {
+    /**
+     * @var \Magento\Framework\Controller\Result\JsonFactory
+     */
     private $resultJsonFactory;
 
+    /**
+     * @var \Magento\Framework\App\RequestInterface
+     */
     private $_request;
 
-    private $_request_test;
-
+    /**
+     * @var \Magento\CatalogInventory\Model\Stock\StockItemRepository
+     */
     protected $_stockItemRepository;
 
-
+    /**
+     * Index constructor.
+     * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \Magento\CatalogInventory\Model\Stock\StockItemRepository $stockItemRepository
+     */
     public function __construct(
         \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
         \Magento\Framework\App\RequestInterface $request,
@@ -25,19 +37,27 @@ class Index implements \Magento\Framework\App\Action\HttpPostActionInterface
         $this->_stockItemRepository = $stockItemRepository;
     }
 
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\Result\Json|\Magento\Framework\Controller\ResultInterface
+     */
     public function execute()
     {
         // TODO: Implement execute() method.
         $product_id = $this->_request->getPost('product_id');
         $result = $this->resultJsonFactory->create();
         if ($product_id) {
-            $result->setData(json_encode(['count'=>$this->getStockItemQty($product_id)]));
+            $result->setData(json_encode(['count' => $this->getStockItemQty($product_id)]));
         } else {
             $result->setData(null);
         }
         return $result;
     }
 
+    /**
+     * @param $productId
+     * @return int
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     private function getStockItemQty($productId)
     {
         $item = $this->_stockItemRepository->get($productId);
