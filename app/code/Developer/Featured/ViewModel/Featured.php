7d3df394e598498de37947a9b18374b7ebda9803
Kopul@ua.fm <Kopul@ua.fm>
2020-11-06 16:52:00 +0200

    End featured list

--- app/code/Developer/Featured/ViewModel/Featured.php
+++ app/code/Developer/Featured/ViewModel/Featured.php
@@ -4,16 +4,66 @@ namespace Developer\Featured\ViewModel;
 
 class Featured implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
-    private $urlBuilder;
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory
+     */
+    private $categoryFactory;
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $_storeManager;
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+     */
+    private $productFactory;
 
     public function __construct(
-        \Magento\Framework\UrlInterface $urlBuilder
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryFactory,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
-        $this->urlBuilder = $urlBuilder;
+        $this->categoryFactory = $categoryFactory;
+        $this->productFactory = $productFactory;
+        $this->_storeManager = $storeManager;
     }
 
-    public function getActionUrl()
+    /**
+     * function Get Categories
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @todo filter all categories on is_featured
+     * @retrun array
+     * @todo Get Categories witch featured in admin panel
+     */
+    private function getCategoriesFeatured()
     {
-        return $this->urlBuilder->getUrl('training_feedback/index/save');
+        $result = $this->categoryFactory->create()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('is_featured', ['eq' => 1 ])
+            ->addAttributeToFilter('level', 2)
+            ->setStore($this->_storeManager->getStore());
+
+        return $result;
+    }
+
+    /**
+     * function Get Products in Categories
+     * @todo Get Products is_featured in categories
+     * @retrun array
+     */
+    public function getProductsFeatured()
+    {
+        $catigories = $this->getCategoriesFeatured();
+        $products = [];
+        foreach ($catigories as $catigory) {
+            $product_to_category = $this->productFactory->create()
+                ->addAttributeToSelect('*')
+                ->addAttributeToFilter('is_featured', ['eq' => 1 ])
+                ->addCategoriesFilter(['eq' => $catigory->getId()])
+                ->setStore($this->_storeManager->getStore());
+
+            $products[$catigory->getId()]['category_info'] = $catigory;
+            $products[$catigory->getId()]['products'] = $product_to_category;
+        }
+        return $products;
     }
 }
